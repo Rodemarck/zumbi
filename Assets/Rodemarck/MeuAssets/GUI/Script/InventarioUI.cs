1ff9659da556edfc674666d9611d41f3c6a4078c
rodemarck <rodemarck.meloj@ufrpe.br>
2019-11-05 12:29:41 -0300

    a

---
+++ Assets/Rodemarck/MeuAssets/GUI/Script/InventarioUI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventarioUI : MonoBehaviour
+{
+    private Inventory inventario;
+    public Transform bolsa;
+    private InventorySlot[] slots;
+    public GameObject inventoryUI;
+    void Start()
+    {
+        inventario = Inventory.instance;
+        inventario.onItemChangedCallback += UpdateUI;
+        slots = bolsa.GetComponentsInChildren<InventorySlot>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+            Cursor.visible = inventoryUI.activeSelf;
+        }
+    }
+
+    // Update is called once per frame
+    void UpdateUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < inventario.items.Count)
+            {
+                slots[i].AddItem(inventario.items[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+    
+    
+}
